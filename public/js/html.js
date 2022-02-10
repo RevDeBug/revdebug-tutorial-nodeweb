86232d175422cfc6c253bfabc668a3d0099e53fa
Tomasz Pytel <tompytel@gmail.com>
2022-02-10 13:35:04 -0300

    setScreenRec()

--- public/js/html.js
+++ public/js/html.js
@@ -8,4 +8,14 @@ function noErrorFunction() {
     console.log('Nothing to see here, move along...');
 }
 
+function startSreenRecord() {
+    console.log('Start screen recording...');
+    revdebug.setScreenRec(true);
+}
+
+function stopSreenRecord() {
+    console.log('Stop screen recording!');
+    revdebug.setScreenRec(false);
+}
+
 console.log('html.js loaded');
